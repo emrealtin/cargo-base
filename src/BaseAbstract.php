51510934aae401d3002df5fc4c1726993f86eda0
emrealtin <emre.altin@flo.com.tr>
2021-11-11 08:43:33 +0300

    interface

--- src/BaseAbstract.php
+++ src/BaseAbstract.php
@@ -1,21 +1,71 @@
 <?php
-
-namespace Vendor\Cargo;
-
-use Cargo\Base\BaseInterface;
+namespace Cargo\Base;
 
 abstract class BaseAbstract implements BaseInterface
 {
-    public function __construct(){}
-    public function auth(){}
-    public function createPackage(){}
-    public function cancelPackage(){}
-    public function getStatus(){}
-    public function doRequest(){}
-    public function setRequest(){}
-    public function getRequest(){}
-    public function setResponse(){}
-    public function getResponse(){}
-    public function setRequestMapping(){}
-    public function getRequestMapping(){}
+    public $config = [];
+
+    public function __construct($config){
+        $this->config = $config;
+    }
+
+   public function auth($params = []): bool
+   {
+       // TODO: Implement auth() method.
+   }
+
+    public function isAuth(): bool
+    {
+        // TODO: Implement isAuth() method.
+    }
+
+    public function createPackage()
+    {
+        // TODO: Implement createPackage() method.
+    }
+
+    public function cancelPackage()
+    {
+        // TODO: Implement cancelPackage() method.
+    }
+
+    public function getStatus()
+    {
+        // TODO: Implement getStatus() method.
+    }
+
+    public function doRequest($request)
+    {
+        // TODO: Implement doRequest() method.
+    }
+
+    public function setRequest()
+    {
+        // TODO: Implement setRequest() method.
+    }
+
+    public function getRequest()
+    {
+        // TODO: Implement getRequest() method.
+    }
+
+    public function setResponse($data = [])
+    {
+        // TODO: Implement setResponse() method.
+    }
+
+    public function getResponse()
+    {
+        // TODO: Implement getResponse() method.
+    }
+
+    public function setRequestMapping($data = []): array
+    {
+        // TODO: Implement setRequestMapping() method.
+    }
+
+    public function getRequestMapping()
+    {
+        // TODO: Implement getRequestMapping() method.
+    }
 }
--- src/BaseInterface.php
+++ src/BaseInterface.php
@@ -1,10 +1,19 @@
 <?php
-
-
 namespace Cargo\Base;
-
 
 interface BaseInterface
 {
+    public function auth($params = []) :bool;
+    public function isAuth() :bool;
+    public function createPackage();
+    public function cancelPackage();
+    public function getStatus();
+    public function doRequest($request);
+    public function setRequest();
+    public function getRequest();
+    public function setResponse($data = []);
+    public function getResponse();
+    public function setRequestMapping($data = []) :array;
+    public function getRequestMapping();
 
 }
